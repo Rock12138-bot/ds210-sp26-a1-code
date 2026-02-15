41b0f87c48c9999ba5cf1f6a7b92e4318342f057
Kevin Shi <shiliuyu@bu.edu>
2026-02-15 00:39:19 -0500

    solution for part2

--- homework_3_guessing_game/src/part2.rs
+++ homework_3_guessing_game/src/part2.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use crate::player::{Player, PlayerTrait};
 use crate::strategies::Strategy;
 
@@ -5,8 +7,25 @@ pub struct Part2 {}
 
 // Terrible strategy: ask if the number is min, otherwise return max.
 impl Strategy for Part2 {
-    fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
-        // YOUR SOLUTION GOES HERE.
-        todo!("No solution provided yet")
+    fn guess_the_number(player: &mut Player, mut min: u32, mut max: u32) -> u32 {
+        
+        //for guess in min..max {
+        let mut mid = min + ((max-min)/2);
+        let remember = player.ask_to_compare(mid);
+            if remember == 0 {
+                return mid
+            } else if remember == -1 {
+                return Part2::guess_the_number(player, min, mid);
+            } else if remember == 1 {
+                return Part2::guess_the_number(player, mid, max);
+            } else {
+                return 0;
+            }
+        //}
+
+    //return 100
     }
+        
+        
 }
+
